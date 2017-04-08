2ced233c9a23ca1667e67517725c973df8695f4f
Kecc16 <kevinjoan16@gmail.com>
2017-04-07 23:48:30 -0500

    (fix) Correcciones menores

--- app/minimax.py
+++ app/minimax.py
@@ -1,17 +1,21 @@
 from abc import ABCMeta, abstractmethod
 
-class Minimax_State(metaclass=ABCMeta):
+class Minimax_State(object):
+    __metaclass__ = ABCMeta
     @abstractmethod
     def sucessors(self):
         pass
-    
+    @abstractmethod
     def is_finished(self):
         pass
+    @abstractmethod
+    def utility(self):
+        pass
 
-class Minimax:
-    def get_next_step(state):
-        pass
-    def max(state):
-        pass
-    def min(state):
-        pass
+def get_next_step(state):
+    #Return max...
+    pass
+def max(state):
+    pass
+def min(state):
+    pass
