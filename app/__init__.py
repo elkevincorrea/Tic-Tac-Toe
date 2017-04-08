e4fb3be38b339bd02ee02a46580362f0ff4efc29
Kecc16 <kevinjoan16@gmail.com>
2017-04-07 23:53:37 -0500

    (refactor) Se cambio el nombre del recurso y la ruta

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_restful import Api
-from app.resources.tic_tac_toe import Tic_Tac_Toe
+from app.resources.tic_tac_toe import Tic_Tac_Toe_Res
 
 app = Flask(__name__, instance_relative_config=True)
 api = Api(app)
 app.config.from_object('config')
 
-api.add_resource(Tic_Tac_Toe, "/Tic-Tac-Toe/<string:board>")
+api.add_resource(Tic_Tac_Toe_Res, "/Tic-Tac-Toe")
