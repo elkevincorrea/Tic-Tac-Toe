6f6408269e1e0e055d454095ebcaaffd1cdae35c
Kecc16 <kevinjoan16@gmail.com>
2017-04-10 23:46:33 -0500

    (feat) Implementación de los metodos de minimax

--- app/minimax.py
+++ app/minimax.py
@@ -2,6 +2,8 @@ from abc import ABCMeta, abstractmethod
 
 class Minimax_State(object):
     __metaclass__ = ABCMeta
+    PLAYER_1 = 1
+    PLAYER_2 = 2
     @abstractmethod
     def sucessors(self):
         pass
@@ -11,11 +13,40 @@ class Minimax_State(object):
     @abstractmethod
     def utility(self):
         pass
+    @abstractmethod
+    def get_player(self):
+        pass
 
 def get_next_step(state):
-    #Return max...
-    pass
-def max(state):
-    pass
-def min(state):
-    pass
+    if state.get_player() == Minimax_State.PLAYER_1:
+        return max_value(state)
+    else:
+        return min_value(state)
+
+def max_value(state):
+    utility = state.utility()
+    if utility != None:
+        return utility, state
+    max_v = -999
+    max_res = None
+    succesors = state.sucessors()
+    for succesor in succesors:
+        min_v, min_res = min_value(succesor)
+        if min_v > max_v:
+            max_v = min_v
+            max_res = succesor
+    return max_v, max_res
+
+def min_value(state):
+    utility = state.utility()
+    if utility != None:
+        return utility, state
+    min_v = 999
+    min_res = None
+    succesors = state.sucessors()
+    for succesor in succesors:
+        max_v, max_res = max_value(succesor)
+        if max_v < min_v:
+            min_v = max_v
+            min_res = succesor
+    return min_v, min_res
